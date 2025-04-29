4b4d39a2f82d4a74769fbf28a5ec2bcb4d8b4bd5
Drosos <drosinakis.drosos@gmail.com>
2025-04-29 05:15:13 +0300

    finish rock paper scissor wiht gui

--- rock-paper-scissors/main.js
+++ rock-paper-scissors/main.js
@@ -1,3 +1,6 @@
+"use strict"
+
+
 function getRandomInteger(min, max) {
 	return Math.floor(Math.random() * (max - min) + min);
 };
@@ -15,58 +18,133 @@ function getComputerChoice() {
 	};
 };
 
-function getHumanChoice() {
-	let choice = prompt("Enter a choice of \n'rock'\n'paper'\n'scissor'")
-	return choice.toLowerCase()
+function reset() {
+	const winnerPopUp = document.querySelector("#winner-box")
+	const playerScore = document.querySelector("#player-score");
+	const roundScore = document.querySelector(".current-score")
+	const computerScore = document.querySelector("#computer-score");
+
+	winnerPopUp.classList.remove("winner-container")
+	winnerPopUp.classList.add("hidden")
+	playerScore.textContent = 0
+	computerScore.textContent = 0
+
+	roundScore.textContent = ""
+	roundScore.classList.remove("win", 'loose', 'draw')
 
 }
 
-function playRound(humanChoice, computerChoice) {
-	if (humanChoice === computerChoice) {
-		return "Draw"
-	}
-	else if (humanChoice === "rock" && computerChoice === "scissor") {
-		return "Player Wins"
-	} else if (humanChoice === "paper" && computerChoice === "rock") {
-		return "Player Wins"
-	} else if (humanChoice === "scissor" && computerChoice === "paper") {
-		return "Player Wins"
 
-	} else {
-		return "Computer Wins"
-	}
+function getGameWinner(playerScore, computerScore) {
 
-
-}
-function main() {
-	let computerScore = 0;
-	let playerScore = 0;
-	for (let i = 0; i < 5; i++) {
-		const result = playRound(getHumanChoice(), getComputerChoice())
-		switch (result) {
-			case "Computer Wins":
-				computerScore++;
-				break;
-			case "Player Wins":
-				playerScore++;
-				break;
-
-
+	if (playerScore >= 5 || computerScore >= 5) {
+		const winnerPopUp = document.querySelector("#winner-box")
+		const winnerText = document.querySelector(".winner")
+		const p = document.createElement("p")
+		const pScore = document.createElement('p')
+		while (winnerText.hasChildNodes()) {
+			winnerText.firstChild.remove()
 		}
-		console.log(result)
-	}
-	console.log("Final Score")
-	if (computerScore > playerScore) {
-		console.log(`Computer wins ${computerScore}:${playerScore}`)
-	} else if (computerScore < playerScore) {
-		console.log(`Player wins ${playerScore}:${computerScore}`)
-	} else {
-		console.log(`Draw ${playerScore}:${computerScore}`)
-	}
 
+		winnerPopUp.classList.remove("hidden")
+		winnerPopUp.classList.add("winner-container")
+		winnerText.classList.remove("win", "loose", "draw")
+		pScore.textContent = `Score: ${playerScore}:${computerScore}`
+
+		if (playerScore === computerScore) {
+
+			p.textContent = "Its a draw"
+			winnerText.appendChild(p)
+			winnerText.appendChild(pScore)
+			winnerText.classList.add("draw")
+
+
+		} else if (playerScore > computerScore) {
+
+			p.textContent = "You Win"
+			winnerText.appendChild(p)
+			winnerText.appendChild(pScore)
+			winnerText.classList.add("win")
+
+		} else {
+			p.textContent = "Game Over"
+			winnerText.appendChild(p)
+			winnerText.appendChild(pScore)
+			winnerText.classList.add("loose")
+		}
+
+	}
+}
+
+
+
+function playRound(playerChoice) {
+	const computerChoice = getComputerChoice();
+	const playerScore = document.querySelector("#player-score");
+	const roundScore = document.querySelector(".current-score")
+	const computerScore = document.querySelector("#computer-score");
+	let playerScoreNumber = Number(playerScore.textContent);
+	let computerScoreNumber = Number(computerScore.textContent);
+
+	if (playerChoice === computerChoice) {
+		playerScoreNumber++;
+		computerScoreNumber++;
+		playerScore.textContent = playerScoreNumber;
+		computerScore.textContent = computerScoreNumber;
+		roundScore.classList.remove("win", 'loose')
+		roundScore.classList.add('draw')
+		roundScore.textContent = "It's a Draw"
+
+	}
+	else if (playerChoice === "rock" && computerChoice === "scissor") {
+		playerScoreNumber++;
+		playerScore.textContent = playerScoreNumber;
+		roundScore.classList.remove("draw", 'loose')
+		roundScore.classList.add("win")
+		roundScore.textContent = "Player wins this Round"
+	} else if (playerChoice === "paper" && computerChoice === "rock") {
+		playerScoreNumber++;
+		playerScore.textContent = playerScoreNumber;
+		roundScore.classList.remove("draw", 'loose')
+		roundScore.classList.add("win")
+		roundScore.textContent = "Player wins this Round"
+	} else if (playerChoice === "scissor" && computerChoice === "paper") {
+		playerScoreNumber++;
+		playerScore.textContent = playerScoreNumber;
+		roundScore.classList.remove("draw", 'loose')
+		roundScore.classList.add("win")
+		roundScore.textContent = "Player wins this Round"
+
+	} else {
+		computerScoreNumber++;
+		computerScore.textContent = computerScoreNumber;
+		roundScore.classList.remove("draw", 'win')
+		roundScore.classList.add("loose")
+		roundScore.textContent = "Computer wins this Round"
+	}
+	getGameWinner(playerScoreNumber, computerScoreNumber)
 
 
 }
 
+
+
+
+function main() {
+
+
+	const resetBtn = document.querySelector(".winner-btn")
+	resetBtn.addEventListener("click", () => {
+		reset()
+	})
+	const playerChoice = document.querySelectorAll(".game-icons")
+	for (const child of [...playerChoice]) {
+		child.addEventListener("click", (e) => {
+			playRound(e.target.id)
+
+		});
+	};
+
+};
 
 main()
