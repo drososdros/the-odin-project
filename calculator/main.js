a37f33f11516d6db8c337b7fdd4595a31ad61e63
Drosos <drosinakis.drosos@gmail.com>
2025-04-30 15:26:11 +0300

    finish the basic func of calc with eval

--- calculator/main.js
+++ calculator/main.js
@@ -1,12 +1,13 @@
 "use strict"
+const buttonList = ["=", "+", "-", "*", "/", "%", "3", "6", "9", ")", ".", "2", "5", "8", "(", "0", "1", "4", "7", "c"]
 function createButtons() {
   /*
    here i create the buttons dynamicaly for the keyboard
    i have reserve order to use the pop and append method
    i use it dynamicaly so i can modify from here the classes they need
+   this app use eval so be carefull only do a basic check
    
    */
-  const buttonList = ["=", "+", "-", "x", "/", "%", "3", "6", "9", ")", ".", "2", "5", "8", "(", "0", "1", "4", "7", "c"]
   const buttons = buttonList.map((btn) => {
     const button = document.createElement("button")
     button.classList.add("btn")
@@ -29,9 +30,68 @@ function createButtons() {
     }
     mainKeyboard.appendChild(keyRow)
 
-
-
   }
 }
+function main() {
+  const input = document.querySelector(".calculate")
+  const keyboard = document.querySelector(".keyboard")
+  const errorMessage = document.querySelector(".error-message")
+  // input.addEventListener("input", () => errorMessage.textContent = "")
+  keyboard.addEventListener("click", (e) => {
+    if (e.target.classList.contains("btn")) {
+      errorMessage.textContent = ""
+      if (e.target.textContent === "c") {
+        input.value = ""
+
+      } else if (e.target.textContent === "=") {
+
+        const result = evaluate(input.value, errorMessage)
+        input.value = ""
+        input.value += result
+      }
+      else {
+        input.value += e.target.textContent
+      }
+
+    }
+  })
+}
+function checkExpresion(s) {
+
+  let strArr = s.split("")
+  for (const letter of strArr) {
+    if (!(buttonList.includes(letter) || letter !== "=")) {
+      throw new Error("not valid expression")
+    }
+
+  }
+  return s
+
+
+
+
+
+
+
+}
+
+function evaluate(s, errorMessage) {
+  try {
+    checkExpresion(s)
+    return eval(s)
+
+  } catch (e) {
+    errorMessage.textContent = "Malformed expression"
+    return
+
+  }
+
+
+
+
+}
+
 
 createButtons()
+main()
+
