5a5d86e1bf520ebd48ce2d5f409ebfd4f1a22a55
Drosos <drosinakis.drosos@gmail.com>
2025-04-30 18:21:21 +0300

    finished the calculator app

--- calculator/main.js
+++ calculator/main.js
@@ -32,11 +32,55 @@ function createButtons() {
 
   }
 }
+function displayeResult(operation, result) {
+  const divContainer = document.createElement("div")
+  const divOperation = document.createElement("div")
+  divOperation.classList.add("operation")
+  divOperation.textContent = operation
+
+  const divAssignment = document.createElement("div")
+  divAssignment.textContent = "="
+  divAssignment.classList.add("assingment")
+  const divResult = document.createElement("div")
+  divResult.classList.add("result")
+  divResult.textContent = result
+  divContainer.classList.add("result-container")
+  divContainer.appendChild(divOperation)
+  divContainer.appendChild(divAssignment)
+  divContainer.appendChild(divResult)
+
+  return divContainer
+
+
+}
 function main() {
+  const allData = document.querySelector("body")
   const input = document.querySelector(".calculate")
   const keyboard = document.querySelector(".keyboard")
   const errorMessage = document.querySelector(".error-message")
+  const passResult = document.querySelector(".past-results")
+  allData.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      const operation = input.value
+      const result = evaluate(input.value, errorMessage)
+      if (result) {
+        input.value = ""
+        input.value += result
+        passResult.appendChild(displayeResult(operation, result))
+
+      }
+
+    }
+  })
   // input.addEventListener("input", () => errorMessage.textContent = "")
+  passResult.addEventListener("click", (e) => {
+    if (e.target.parentNode.classList.contains("result-container")) {
+      input.value = ""
+      input.value = e.target.parentNode.firstChild.textContent
+
+    }
+
+  })
   keyboard.addEventListener("click", (e) => {
     if (e.target.classList.contains("btn")) {
       errorMessage.textContent = ""
@@ -45,9 +89,14 @@ function main() {
 
       } else if (e.target.textContent === "=") {
 
+        const operation = input.value
         const result = evaluate(input.value, errorMessage)
-        input.value = ""
-        input.value += result
+        if (result) {
+          input.value = ""
+          input.value += result
+          passResult.appendChild(displayeResult(operation, result))
+
+        }
       }
       else {
         input.value += e.target.textContent
@@ -60,10 +109,9 @@ function checkExpresion(s) {
 
   let strArr = s.split("")
   for (const letter of strArr) {
-    if (!(buttonList.includes(letter) || letter !== "=")) {
+    if (!buttonList.includes(letter) || letter === "=") {
       throw new Error("not valid expression")
     }
-
   }
   return s
 
@@ -77,7 +125,7 @@ function checkExpresion(s) {
 
 function evaluate(s, errorMessage) {
   try {
-    checkExpresion(s)
+    s = checkExpresion(s)
     return eval(s)
 
   } catch (e) {
