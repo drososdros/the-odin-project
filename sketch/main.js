004ecb3c6d9c618ec493ea89cd2d6eed63d595da
Drosos <drosinakis.drosos@gmail.com>
2025-04-30 04:15:15 +0300

    finish sketch

--- sketch/main.js
+++ sketch/main.js
@@ -1,11 +1,11 @@
 "use strict"
-function drawContent() {
+function drawContent(value) {
   const container = document.querySelector(".main-container");
-  for (let j = 0; j < 15; j++) {
+  for (let j = 0; j <= value; j++) {
 
     const mainDiv = document.createElement("div");
     mainDiv.classList.add("collumn")
-    for (let i = 0; i < 15; i++) {
+    for (let i = 0; i <= value; i++) {
       const div = document.createElement("div");
       div.classList.add("collumn-item")
 
@@ -16,10 +16,46 @@ function drawContent() {
   }
 }
 
+function resetGrid(gridSelector) {
 
-drawContent()
+  while (select.hasChildNodes()) {
+    select.firstChild.remove()
+
+  }
+  drawContent(gridSelector.value)
+}
+
+
+
+drawContent(8)
 const select = document.querySelector(".main-container")
-select.addEventListener("click", (e) => {
-  console.log(e.target)
+const color = document.querySelector("#color")
+const resetBtn = document.querySelector(".reset-btn")
+const gridSelector = document.querySelector("#grid-selector")
+const gridValue = document.getElementById("grid-value")
+gridSelector.value = 8
+gridValue.textContent = "8x8"
+
+select.addEventListener("mouseover", (e) => {
+  if (!e.target.classList.contains("collumn") && !e.target.classList.contains("main-container")) {
+    e.target.style.backgroundColor = color.value
+    e.target.style.border = `.1px solid ${color.value}`
+
+  }
+
+})
+
+resetBtn.addEventListener("click", () => {
+  if (resetBtn.textContent !== "reset") {
+    resetBtn.textContent = "reset"
+
+  }
+  resetGrid(gridSelector)
+})
+
+gridSelector.addEventListener("input", (e) => {
+
+  gridValue.textContent = `${e.target.value}x${e.target.value}`
+  resetBtn.textContent = "Change grid"
 
 })
