d3fb84f3dc55e185a9cc29739866c1c790d12dd6
Drosos <drosinakis.drosos@gmail.com>
2025-05-23 10:12:17 +0300

    create the Todo class

---
+++ todo-app/src/ts/todo/todo.ts
@@ -0,0 +1,68 @@
+import { TodoType } from "./types"
+import { Task } from "./task"
+
+
+export class Todo implements TodoType {
+  static #_id = 1
+  #id: number
+  title: string
+  date: Date
+  isCompleted: boolean
+  tasks: Task[]
+  constructor(
+    title: string,
+    date: Date,
+    tasks: Task[] = [],
+  ) {
+    this.#id = Todo.#_id
+    this.title = title
+    this.date = date
+    this.isCompleted = false
+    this.tasks = tasks
+    Todo.#_id++
+  }
+  get id() {
+    return this.#id
+  }
+  add(todo: Task) {
+    this.tasks.push(todo)
+  }
+  remove(id: number) {
+    this.tasks = this.tasks.filter((todo: Task) => todo.id !== id)
+  }
+  setIsCompleted(isCompleted: boolean = true) {
+    this.isCompleted = isCompleted
+    for (const task of this.tasks) {
+      task.setIsCompleted(isCompleted)
+    }
+  }
+  getTask(id: number) {
+    return this.tasks.find(task => task.id === id)
+  }
+
+}
+
+
+
+const item = new Task({ name: "name", isCompleted: false, priority: 1 })
+const item2 = new Task({ name: "name", isCompleted: false, priority: 1 })
+const item3 = new Task({ name: "name", isCompleted: false, priority: 1 })
+
+console.log(item.id, item2.id, item3.id);
+const todo = new Todo("shoping list", new Date())
+console.log(todo);
+
+todo.add(item)
+todo.add(item2)
+todo.add(item3)
+console.log(todo);
+todo.remove(item.id)
+console.log(todo);
+
+const obj = todo.getTask(item2.id)
+console.log(obj);
+obj?.update({ name: "hello world" })
+console.log(todo);
+
+
+
